2db83603f88e7c63fe645ccf7eb551956bb827c4
«roma <teterdynko_roman@mail.ru>
2022-07-17 03:23:17 +0300

    task008

---
+++ task008/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+int a = new Random().Next(1, 99);
+Console.WriteLine(a);
+int i = 1;
+while (i <= a)
+{
+    if (i % 2 == 0)
+        Console.Write(i);
+    Console.Write(" ");
+    i++;
+}
