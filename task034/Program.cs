4578392dac9bad23caa64a179df4f4f2c3f7181f
«roma <teterdynko_roman@mail.ru>
2022-08-16 01:53:32 +0300

    task034

---
+++ task034/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+
+Console.WriteLine("Введите количество элементов массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[num];
+int col = 0;
+for (int i = 0; i < num; i++)
+{
+    array[i] = new Random().Next(100,1000);
+    if (array[i] % 2 == 0) col++;
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine(col);
