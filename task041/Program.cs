66181349142bb4210ffc3fabb37aff7d92fa630e
«roma <teterdynko_roman@mail.ru>
2022-08-19 00:26:23 +0300

    task041

---
+++ task041/Program.cs
@@ -0,0 +1,12 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите количество элементов: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < num; i++)
+{
+    Console.Write("Введите элемент: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number > 0) count++;
+}
+Console.WriteLine($"Количество положительных введенных чисел: {count}");
