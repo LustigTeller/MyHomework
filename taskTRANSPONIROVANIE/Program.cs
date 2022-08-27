35938b322cc70e42b738a3c5d9a61af67cefa0e0
«roma <teterdynko_roman@mail.ru>
2022-08-27 18:16:14 +0300

    taskTRANSPONIROVANIE

---
+++ taskTRANSPONIROVANIE/Program.cs
@@ -0,0 +1,39 @@
+﻿// Первая строка входного файла INPUT.TXT содержит натуральное число N – количество строк и столбцов матрицы.
+// В каждой из последующих N строк записаны N целых чисел – элементы матрицы. 
+// Все числа во входных данных не превышают 100 по абсолютной величине.
+
+
+Console.Write("Введите число N - количество строк и столбцов матрицы: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[num, num];
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = new Random().Next(1, 11);
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        if (i > j)
+        {
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[j, i];
+            matrix[j, i] = temp;
+        }
+    }
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+        Console.Write(matrix[i, j] + "\t");
+    Console.WriteLine();
+}
