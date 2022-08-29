f88257f06f218378659651d7f0b8684542a48151
«roma <teterdynko_roman@mail.ru>
2022-08-29 16:38:57 +0300

    task060

---
+++ task060/Program.cs
@@ -0,0 +1,45 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+
+
+void NewMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+                matrix[i, j, k] = new Random().Next(10, 100);
+        }
+    }
+}
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.WriteLine(matrix[i, j, k] + "(" + i + ", " + j + ", " + k + ")");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
+Console.Write("Введите X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int[,,] matrixOne = new int[x, y, z];
+NewMatrix(matrixOne);
+PrintMatrix(matrixOne);
